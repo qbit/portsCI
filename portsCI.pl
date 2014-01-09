d988218d1d94194e15c2b7f87f9e5fe0047bc9d9
Aaron Bieber <deftly@gmail.com>
2014-01-09 07:05:07 -0700

    portsCI.pl is basically my test bed right now

--- portsCI.pl
+++ portsCI.pl
@@ -9,10 +9,26 @@ use lib 'lib';
 use PortsCIDB;
 
 my $opts = {
-	debug => 1 #,
-	#dbfile => 'test.db'
+	debug => 1,
+	db => 'test.db'
 };
 
 my $db = new PortsCIDB($opts);
 
-$db->dpop();
+#$db->dpush('lang/node');
+#$db->dpush('lang/libv8');
+
+warn Dumper $db->{store};
+
+# while (1) {
+	# my $port = $db->dshift();
+	# if ($port) {
+	# 	print "Building: $port\n";
+	# } else {
+	# 	print "waiting for more stuff\n";
+	# 	sleep 10;
+	# 	$db->dpush('productivity/taskwarrior');
+	# }
+# }
+
+$db->finish();
